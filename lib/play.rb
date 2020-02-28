1d14762236543ffb1c1327a5a91c2c9c1e1908eb
Barbara Bontempo <barbarabontempo94@gmail.com>
2020-02-28 21:47:01 +0000

    Automatically backed up by Learn

--- lib/play.rb
+++ lib/play.rb
@@ -37,9 +37,9 @@ end
 
 # Define your play method below
 def play(board)
-  more_turns = 0
-  until more_turns == 9
-    more_turns+1
-    turn(board)
-  end
+i = 0
+while i < 9
+  i += 1
+  turn(board)
+end
 end
